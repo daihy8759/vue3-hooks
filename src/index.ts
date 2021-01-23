9aecee8080917c6a501c2bf674b4063fc6350712
daihy <dai875939260@163.com>
2021-01-23 10:37:13 +0800

    :sparkles: Add useEventListener

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { useDebounce } from './hooks/core/useDebounce';
 export { useThrottle } from './hooks/core/useThrottle';
 export { useTimeoutFn } from './hooks/core/useTimeout';
+export { useEventListener } from './hooks/event/useEventListener';
 export { useECharts } from './hooks/web/useECharts';
